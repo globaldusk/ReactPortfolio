fb5be34691b1c08ebbb3900574d3b624512353f3
Jack Shakespeare <globaldusk@gmail.com>
2023-06-30 13:50:23 +1000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className="text-gray">
           <div className="footer-container">
             <p>All rights reserved ©</p>
-            <a href="https://github.com/globaldusk" target="_blank"><AiFillGithub className="icon" size="2rem"/></a>
+            <a href="https://github.com/globaldusk" target="_blank" className="icon"><AiFillGithub  size="2rem"/></a>
             <a href="https://www.linkedin.com/in/jack-shakespeare/" className="icon" target="_blank"><AiFillLinkedin size="2rem"/></a>
           </div>
         </div>
